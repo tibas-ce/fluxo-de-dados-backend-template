3501937905ef2e6fbdc3efcc9b72223ecec46985
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2023-03-20 22:32:07 -0300

    Atividades em aula

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,156 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import { accounts } from './database'
-import { ACCOUNT_TYPE } from './types'
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { accounts } from './database';
+import { ACCOUNT_TYPE } from './types';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003")
-})
+    console.log("Servidor rodando na porta 3003");
+});
 
 app.get("/ping", (req: Request, res: Response) => {
-    res.send("Pong!")
-})
+    res.send("Pong!");
+});
 
 app.get("/accounts", (req: Request, res: Response) => {
-    res.send(accounts)
-})
+    res.send(accounts);
+});
 
+//Refatorando getAccountsById com try/catch
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try{
+        const id = req.params.id;
 
-    const result = accounts.find((account) => account.id === id) 
+        const result = accounts.find((account) => account.id === id);
+        
+        if(!result){
+            res.status(404);
+            //res.statusCode = 404;
+            throw new Error("Conta não encontrada. Verifique a 'id'.");
+        };
 
-    res.status(200).send(result)
-})
+        res.status(200).send(result);
+    } catch(error){
+        console.log(error);
+        if(res.statusCode === 200){
+            res.status(500);
+        };
+        res.send(error.message);        
+    };
+});
 
+//Refatorando deleteAccount com try/catch
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try{
+        const id = req.params.id;
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+        if(id[0] !== "a"){
+            res.status(404);
+            throw new Error("'id' inválido.Deve iniciar com a letra 'a'.");
+        }; 
 
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
-    }
+        const accountIndex = accounts.findIndex((account) => account.id === id);
 
-    res.status(200).send("Item deletado com sucesso")
-})
+        if (accountIndex >= 0) {
+            accounts.splice(accountIndex, 1);
+            res.status(200).send("Item deletado com sucesso");
+        } else {
+            res.status(404).send("Conta não existente. Verifique o 'id'.");
+        };
+    } catch(error){
+        console.log(error);
+        if(res.statusCode === 200){
+            res.status(500);
+        };
+        res.send(error.message);
+    };
+});
 
+//Refatorando editAccount com try/catch
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try{
+        const id = req.params.id;
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+        const newId = req.body.id;
+        const newOwnerName = req.body.ownerName;
+        const newBalance = req.body.balance;
+        const newType = req.body.type;
 
-    const account = accounts.find((account) => account.id === id) 
+        if(id !== undefined){
+            if(id[0] !== "a"){
+                res.status(404);
+                throw new Error("'id' inválido. Deve iniciar com a letra 'a'.");
+            };
+        };
+        
+        if(newId !== undefined){
+            if(typeof newId !== "string"){
+                res.status(404)
+                throw new Error("'newId' deve ser uma 'string'.");
+            };
 
-    if (account) {
-        account.id = newId || account.id
-        account.ownerName = newOwnerName || account.ownerName
-        account.type = newType || account.type
+            if(newId[0] !== "a"){
+                res.status(404);
+                throw new Error("'id' inválido. Deve iniciar com a letra 'a'.");
+            };
+        };
 
-        account.balance = isNaN(newBalance) ? account.balance : newBalance
+        
+        
+        if(newOwnerName !== undefined){
+            if(typeof newOwnerName !== "string"){
+                res.status(404)
+                throw new Error("'newOwnerName' deve ser uma 'string'.");
+            };
+            
+            if(newOwnerName.length < 2){
+                res.status(404);
+                throw new Error("Seu ownerName deve conter no mínimo dois caracteres.");
+            };
+        };
+        
+        if(newBalance !== undefined){
+            if(typeof newBalance !== "number"){
+                res.status(400);
+                throw new Error("'balance' dedve ser do tipo 'number'.");
+            };
+            if(newBalance < 0 ){
+                res.status(400);
+                throw new Error("'balance' deve ter valor maior ou igual a zero.");
+            };
+        };
+
+        if(newType !== undefined){
+            if(
+                newType !== ACCOUNT_TYPE.GOLD &&
+                newType !== ACCOUNT_TYPE.BLACK && 
+                newType !== ACCOUNT_TYPE.PLATINUM){
+                res.status(400);
+                throw new Error("'type' deve ser um tipo válido: Ouro, Platina ou Black.");
+            };
+        };
+
+        const account = accounts.find((account) => account.id === id);
+
+        if (account) {
+            account.id = newId || account.id;
+            account.ownerName = newOwnerName || account.ownerName;
+            account.type = newType || account.type;
+
+            account.balance = isNaN(newBalance) ? account.balance : newBalance;
+        };
+
+        res.status(200).send("Atualização realizada com sucesso");
+    } catch(error) {
+        console.log(error);
+        if(res.statusCode === 200){
+            res.status(500);
+        };
+        res.send(error.message);
     }
-
-    res.status(200).send("Atualização realizada com sucesso")
-})
+    
+});
